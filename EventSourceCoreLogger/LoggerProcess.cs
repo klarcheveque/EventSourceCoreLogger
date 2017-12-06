d8dd9e66599088453bad834843e0e1eb02b42389
Kéven L'Archevêque <klarcheveque@gmail.com>
2017-12-06 00:18:32 -0500

    Add LoggerProcess to link the EventsourceLogger and the SinkRegister

---
+++ EventSourceCoreLogger/LoggerProcess.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.Tracing;
+using EventSourceCoreLogger.Registration;
+
+namespace EventSourceCoreLogger
+{
+    public class LoggerProcess
+    {
+        private static EventSource _eventSource;
+        private static IEventSourceRegistrer _eventSourceRegistrer;
+
+        public static void Start(EventSource eventSource)
+        {
+            _eventSource = eventSource;
+
+            _eventSourceRegistrer = new SingleFileRegistration();
+            _eventSourceRegistrer.Registrer(_eventSource);
+        }
+
+        public static void Stop()
+        {
+            _eventSourceRegistrer.Dispose();
+
+            _eventSource.Dispose();
+
+        }
+    }
+}
